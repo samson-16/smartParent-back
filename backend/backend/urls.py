9efbab6f31738e40086f25eb229c341109011b9b
YEABSIRA YIRDAW <yeabsirayirdaw@gmail.com>
2024-04-09 13:41:45 -0900

    teacherapp

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path , include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from core_app.views import CreateUserView
-
+from teacher.views import ExamResultListCreate,AssignmentListCreate
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,5 @@ urlpatterns = [
     path ('api/token/refresh/', TokenRefreshView.as_view(), name="refresh_token"  ),
     path ('api-auth/', include("rest_framework.urls")),
     path ('api/', include("core_app.urls")),
+    path ('api/', include("teacher.urls")),
 ]
